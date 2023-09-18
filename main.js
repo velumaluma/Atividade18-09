c5153239ca267be39dd12a55d99eb1f5d34f06c6
velumaluma <144016672+velumaluma@users.noreply.github.com>
2023-09-18 08:57:49 -0300

    Add files via upload
    Construindo Botão

---
+++ main.js
@@ -0,0 +1,14 @@
+const btnmudar = document.getElementById("mudar");
+btnmudar.addEvemtListener("click")
+const content =  document.getElementById("content");
+fetch("https://dog.ceo/api/breeds/image/random")
+     .then(res => res.json())
+     .then(res => {
+         const img = document.createElement("img");
+         img.src = res.message
+         content.appendChild(img)
+     })
+     .catch(err => {
+         console.log(err)
+     })
+
